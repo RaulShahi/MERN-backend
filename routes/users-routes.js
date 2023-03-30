d4688aa8bdcad830210c59df63409bc841ee8f4e
RaulShahi1 <shahi.raul@webexpertsnepal.com>
2023-03-30 17:41:05 +0545

    added multer middlerware

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const userController = require("../controllers/user-controllers");
 const { verifyToken } = require("../middlewares/verifyToken");
+const fileUpload = require("../middlewares/file-upload");
 
 const router = express.Router();
 
 router.route("/").get(userController.getAllUsers);
-router.post("/signup", userController.registerUser);
+router.post("/signup", fileUpload.single("image"), userController.registerUser);
 router.post("/login", userController.loginUser);
 router.delete("/:uid", verifyToken, userController.deleteUser);
 
